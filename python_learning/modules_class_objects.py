cb91899d2eead57d2b2cdadec103f7bd041a6c0f
cflagg <cflagg@neoninc.org>
2015-11-05 09:51:32 -0700

    some javascript and python learning code

---
+++ python_learning/modules_class_objects.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 05 09:45:56 2015
+
+@author: cflagg
+
+Modules, Classes, Objects
+http://learnpythonthehardway.org/book/ex40.html
+"""
+
+# modules are like dictionaries
+# get X from Y, a simple index list or 'table'
+dict = {'apple' : "I AM A FRUIT", 'tomato' : "I AM ALSO A FRUIT", "carrot" : "I AM A VEGETABLE"}
+
